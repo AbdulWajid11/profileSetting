c99dd9b8a75426ffcdb1fe3db08bcb87a0bc3b04
AbdulWajid11 <awajidkhan49@gmail.com>
2022-01-04 23:47:48 +0500

    input outline

--- src/components/wallet/payout/index.jsx
+++ src/components/wallet/payout/index.jsx
@@ -31,24 +31,24 @@ const Payout = () => {
   return (
     <div>
       {/* background_color_div */}
-      <div className="background_color_div">
+      <div className="background_color_div flex justify-center">
         {/* main_container */}
-        <div className="container">
+        <div className="w-3/4">
           {/* heading_hr_line */}
-          <div className="heading_hr_line">
-            <h2 className="text-white text-7xl font-normal pb-6">Wallet</h2>
+          <div className="heading_hr_line w-full lg:w-4/5">
+            <h2 className="text-white text-3xl lg:text-7xl font-normal text-center lg:text-left pt-4 pb-4  lg:pb-6">Wallet</h2>
             <hr className="pt-6" />
           </div>
           {/* upside_content_div */}
-          <div className="upside_content_div flex">
+          <div className="upside_content_div flex flex-col lg:flex-row lg:flex-row">
             {/* left_side_content */}
-            <div className="common_bg_color flex justify-center items-center mr-4 lg:w-1/3 rounded-xl">
+            <div className="common_bg_color flex justify-center items-center mb-4 lg:mb-0 mr-0 lg:mr-4 w-full lg:w-1/3 rounded-xl">
               {/* content_div */}
               <div>
                 {/* heading_text */}
                 <div>
-                  <h2 className="text-white text-center text-6xl">12 EUR</h2>
-                  <p className="text-white text-center text-sm">
+                  <h2 className="text-white text-center text-3xl  lg:text-6xl">12 EUR</h2>
+                  <p className="text-white text-center text-xs lg:text-sm">
                     min payout: 50 EUR
                   </p>
                 </div>
@@ -61,7 +61,7 @@ const Payout = () => {
               </div>
             </div>
             {/* right_side_div */}
-            <div className=" common_bg_color rounded-xl lg:w-3/4">
+            <div className=" common_bg_color rounded-xl  w-full lg:w-3/4">
               {/* content_div */}
               <div className="pt-4">
                 {/* heading */}
@@ -71,10 +71,10 @@ const Payout = () => {
                   </h2>
                 </div>
                 {/* lists_main_div */}
-                <ul className="lists_main_div flex px-4  pt-4 pb-2">
+                <ul className="lists_main_div flex flex-col lg:flex-row  px-0  lg:px-4  pt-4 pb-2">
                   {lists?.map((single) => (
-                    <ul className="px-6">
-                      <li className="common_li text-center text-lg font-medium">
+                    <ul className="py-2 lg:py-0 px-0 lg:px-4">
+                      <li className="common_li text-center text-xl font-medium">
                         {single.title}
                       </li>
                       <li className="common_li text-base font-medium text-center leading-9">
